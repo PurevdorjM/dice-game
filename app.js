e9eaed14a658efc6c129a6c2036a5498da703e51
puujeest <puujeest@gmail.com>
2020-02-12 18:31:23 +0800

    Event gej yuve hicheeliin daguu nemelt hiilee

--- app.js
+++ app.js
@@ -9,19 +9,21 @@ var roundScore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
 
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-//<div class="player-score" id="score-0">43</div>
-// window.document.querySelector("#score-0").textContent = dice;
+// Программ эхлэхэд бэлтгэе
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
 
-// document.querySelector("#score-1").innerHTML = "<em>Yes!<em>";
-
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
 document.querySelector(".dice").style.display = "none";
 
-console.log("Шоо:" + dice);
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  document.querySelector(".dice").style.display = "block";
+  document.querySelector(".dice").style.display = "block";
+
+  alert("Шоог шидлээ: " + diceNumber);
+});
